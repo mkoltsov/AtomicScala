0bd9ba03abe29b15ef35fcc79c1ffe19fb7893d4
mkoltsov <root@javabean.ru>
2014-12-31 05:59:55 -0800

    Atom2 completed

--- Basics/Classes.scala
+++ Basics/Classes.scala
@@ -1,4 +1,10 @@
-class Mammal{
+import java.util.{Random => Rnd}
+
+class Mammal {
+  def meow(cnt:Int) = {
+    "i'm a cat " + cnt
+  }
+  println("instantiating")
   val immut = "123"
 
   var mut = 123
@@ -7,3 +13,24 @@ class Mammal{
 val inst = new Mammal
 
 println(inst.immut)
+println(inst.meow(1))
+
+val rnd = new Rnd()
+
+println(rnd.nextInt(100))
+
+class Royalty(clazz:String, chefValue:Boolean){
+  var partOfRoyalFamily:Boolean = _
+  val id = rnd.nextInt(100)
+  println("new royalty has been added",clazz, chefValue, partOfRoyalFamily, id)
+  def makeMeSir={println(clazz+" has been promoted to Sir")}
+
+}
+
+val chef = new Royalty("chef", true)
+chef.partOfRoyalFamily = true
+new Royalty("pupa", false)
+chef.makeMeSir
+//class Animal(cnt:Int, name:String){
+//  var cnt
+//}

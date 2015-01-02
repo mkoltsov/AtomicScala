272297cf5d4891a3eb7b7d0768fa8a0a18ec5d8d
mkoltsov <root@javabean.ru>
2015-01-02 12:22:17 +0300

    Atom3 in progress

--- Basics/Classes.scala
+++ Basics/Classes.scala
@@ -34,3 +34,36 @@ chef.makeMeSir
 //class Animal(cnt:Int, name:String){
 //  var cnt
 //}
+
+class RndClass1(val arg:Int)
+class RndClass2(var arg:Int)
+
+val rnd1 = new RndClass1(3)
+val rnd2 = new RndClass2(6)
+println(rnd1.arg)
+rnd2.arg = 10
+println(rnd2.arg)
+
+class VarArg(arg:Int*){
+  for (i<-arg) println(i)
+}
+
+println(new VarArg(1,2,3,4,5,6,7))
+//named args
+class Named(chefValue:Boolean, pupaValue:Boolean, amount: Double){
+  println(chefValue, pupaValue, amount)
+}
+
+println(new Named(pupaValue = false, chefValue = true, amount = 13.2))
+
+//default atgs
+
+class DefaultArgs(superValue:Boolean, loserValue:Boolean=false, amount: Double=20.02){
+  println(superValue, loserValue, amount)
+}
+
+println(new DefaultArgs(superValue = true))
+
+class NamedVarArg(mom:Boolean, dad:Boolean,kids:String*)
+
+val chef1 = new NamedVarArg(mom = true, dad= false, kids = "323","323")

52169eaa05d66719ef25d67e1b812c0350fd2ec2
mkoltsov <root@javabean.ru>
2015-01-09 00:40:28 +0300

    override, automatic string conversion examples added

--- Advanced/ABitOfStyle.scala
+++ Advanced/ABitOfStyle.scala
@@ -2,6 +2,7 @@
 case class Style(va: Boolean) {
   //when a method without the args mutates the state it's convenient to write it with parenthesis
   def getA() = va
+
   //if it doesn't mutate, it's common to write the method without parenthesis
   def getB = va
 }
@@ -11,3 +12,25 @@ val st = Style(true)
 //st.getA()
 //it's better to write like this
 st.getB
+
+case class Stub() {
+  def +(x: Any) = {
+    s"adding $x"
+  }
+
+  def -(x: Any) = {
+    s"subtracting $x"
+  }
+}
+
+val ins = Stub()
+
+println(ins - 1)
+println(ins + 1)
+
+
+class Chef{
+  override def toString = "chef's output"
+}
+
+println (new Chef)
